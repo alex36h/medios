dc236da8bf42beee98aff813fa1c71d75ad0516e
alex36h <a.kinglex@hotmail.com>
2020-04-15 16:30:38 -0600

    navbar

--- inc/header.php
+++ inc/header.php
@@ -83,3 +83,35 @@
 
 <!-------------------------------------------->
 <div class="container">
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary ">
+  <a class="navbar-brand" href="#">M&R</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Medios</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Geo Marketing</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
